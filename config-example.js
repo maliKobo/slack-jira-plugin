3d667b2dac15e99b931576115dd2d33314f081fa
Muhammad Ali <mali@kobo.com>
2016-04-21 11:50:01 -0400

    Set some defaults in the configuration file that apply to Kobo

--- config-example.js
+++ config-example.js
@@ -5,38 +5,23 @@ var config = {
   showIssueDetails: true,
   issueDetailsToShow: {'fields.summary':1 , 'fields.assignee' : 1, 'fields.description': 0},
   showDetailsByDefault: true,//if true, you don't need the '+' to get details
-  bot_name: "jira",//Provide the name to post under
-  token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX', // https://api.slack.com/web
+  bot_name: "JIRA",//Provide the name to post under
+  token: '', // https://api.slack.com/web
   jira_urls: {
     // DEFAULT NODE IS REQUIRED.
-    "DEFAULT": {url: "https://default.jira.server/browse/"},
-    // These should match projects from the projects property where you want to use a configuration other than the default
-    "SOLR": {
-      url: "https://issues.apache.org/jira/browse/",
-      jira: {
-        user: 'username', // be sure to use the username, not the user email
-        password: 'password',
-        host: 'hostname',
-        protocol: 'https',
-        port: 443,
-        version: '2',
-        verbose: true,
-        strictSSL: true
-      }
-    },
-    "GRANT": {url:"http://grant.jira.server/jira/browse/"}
+    "DEFAULT": {url: "https://feedback.kobobooks.com/browse/"}
   },
   search_cmd: "search",
   //Since search results can be verbose, you may not want to muddy the channel
-  search_output_chan: "C02U1L9KZ",//if the value is "this", then the current channel will be used, else the name of a channel
-  projects: ["REPLACE", "ME", "WITH", "YOUR", "PROJECT", "NAMES", "GRANT", "SOLR"],
-  post: true,
+  search_output_chan: "this",//if the value is "this", then the current channel will be used, else the name of a channel
+  projects: ["REPLACE", "ME", "WITH", "YOUR", "PROJECT", "NAMES", "ADRD"],
+  post: false,
   verbose: true,
-  emoji: ":jira:", // be sure to upload your custom emoji in slack
+  emoji: ":koby:", // be sure to upload your custom emoji in slack
   link_separator: ", ",// use \n if you want new lines
   error_channel: '', //the id of the channel to send low level log errors.  If not defined, will use the current channel
   
-  baseurl: "",
+  baseurl: "https://feedback.kobobooks.com/",
   username: '',
   password: ''
 };
